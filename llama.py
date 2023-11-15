f74cb16e37f317b349dcfeb172fb37923351bd32
Tim Stokman <timstokman@gmail.com>
2023-11-15 11:57:00 +0100

    Tweak llama checkpoint

--- llama.py
+++ llama.py
@@ -1,5 +1,5 @@
 from llama_cpp import Llama
 
-llm = Llama(model_path="./llama-2-7b.Q8_0.gguf")
+llm = Llama(model_path="./llama-2-7b-chat.Q8_0.gguf")
 output = llm("Question: Name the planets in the solar system? Answer: ", max_tokens=1024, stop=['\n'], echo=True)
 print(output)
